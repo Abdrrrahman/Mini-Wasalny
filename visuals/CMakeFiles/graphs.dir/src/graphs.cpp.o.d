f621ca17612f176629c47c457577f545db8256f0
Abdrrrahman <abdrahman24sc@gmail.com>
2026-05-21 20:05:38 +0300

    Edited some of raylib submodule

--- visuals/CMakeFiles/graphs.dir/src/graphs.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/graphs.dir/src/graphs.cpp.o: \
- /media/null/Common1/Projects/DS-Project-Graphs/visuals/src/graphs.cpp \
- /usr/include/stdc-predef.h \
- /media/null/Common1/Projects/DS-Project-Graphs/visuals/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/14/include/stdarg.h
